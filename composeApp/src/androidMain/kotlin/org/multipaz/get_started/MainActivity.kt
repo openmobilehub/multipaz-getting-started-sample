1ec7275f456d556f756a2e4189e9ab74d16cbf3b
Vishnu Sanal T <vishnu@openmobilehub.org>
2025-07-17 21:13:12 +0530

    UI Prompt Handling with PromptModel
    Signed-off-by: Vishnu Sanal T <vishnu@openmobilehub.org>

--- composeApp/src/androidMain/kotlin/org/multipaz/get_started/MainActivity.kt
+++ composeApp/src/androidMain/kotlin/org/multipaz/get_started/MainActivity.kt
@@ -1,26 +1,22 @@
 package org.multipaz.get_started
 
 import android.os.Bundle
-import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.fragment.app.FragmentActivity
 import org.multipaz.context.initializeApplication
+import org.multipaz.prompt.AndroidPromptModel
 
-class MainActivity : ComponentActivity() {
+// IMPORTANT: Multipaz's PromptDialogs require the activity to be a FragmentActivity
+// to support the BiometricPrompt and other platform features.
+class MainActivity : FragmentActivity() { // use FragmentActivity
+    val promptModel = AndroidPromptModel()
     override fun onCreate(savedInstanceState: Bundle?) {
         enableEdgeToEdge()
         super.onCreate(savedInstanceState)
         initializeApplication(this.applicationContext)
         setContent {
-            App()
+            App(promptModel)
         }
     }
-}
-
-@Preview
-@Composable
-fun AppAndroidPreview() {
-    App()
 }
--- composeApp/src/iosMain/kotlin/org/multipaz/get_started/MainViewController.kt
+++ composeApp/src/iosMain/kotlin/org/multipaz/get_started/MainViewController.kt
@@ -1,5 +1,8 @@
 package org.multipaz.get_started
 
 import androidx.compose.ui.window.ComposeUIViewController
+import org.multipaz.prompt.IosPromptModel
 
-fun MainViewController() = ComposeUIViewController { App() }
+fun MainViewController() = ComposeUIViewController {
+    App(IosPromptModel())
+}
